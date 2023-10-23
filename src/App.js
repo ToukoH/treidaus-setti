de1ac36fc9257d97a9663d56c098df80308acb36
Touko Haapanen <toukoh@Touko-MacBook-Pro.local>
2023-10-23 15:38:17 +0300

    pepsi max

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react'
 import StockChart from './StockChart'
+import axios from 'axios'
+
+const url = 'http://127.0.0.1:8000/getJsonFile/pliisom.json'
+
+axios({
+  method: 'get',
+  url
+}).then(function (response) {
+  console.log(response.data)
+}).catch(function (error) {
+  console.error('Error fetching data: ', error)
+})
 
 function App () {
-  // NEXT TRY TO FETCH DATA FROM API AND USE IT HERE
   const data = [
     { date: '01-01-2023', price: 220 },
     { date: '02-01-2023', price: 230 },
