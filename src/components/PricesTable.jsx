2ca88f998ada0c7250214f3bae6e5a0c53061b8f
Touko Haapanen <toukoh@Touko-MacBook-Pro.local>
2023-10-29 20:06:25 +0200

    stock chart

--- src/components/PricesTable.jsx
+++ src/components/PricesTable.jsx
@@ -1,20 +1,70 @@
-import React from 'react'
-import getFile, { res } from '../utilities/fetchPrices'
-import { Button, Box, Heading } from '@chakra-ui/react'
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts'
+import React, { useEffect, useState } from 'react'
+import {
+    LineChart,
+    Line,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    Legend,
+    ResponsiveContainer
+} from 'recharts'
+import { Box, Heading } from '@chakra-ui/react'
+import { res } from '../utilities/fetchPrices'
 
-function PricesTable () {
-    const handlePress = () => {
-        console.log(res)
-    }
+const StockChart = () => {
+    const [chartData, setChartData] = useState([])
+
+    useEffect(() => {
+        if (res && Object.keys(res).length > 0) {
+            const formattedData = Object.entries(res).map(([date, data]) => {
+                return {
+                    date: date.substring(0, 10),
+                    close: data.Close
+                }
+            })
+            setChartData(formattedData)
+        }
+    }, [res])
 
     return (
-        <div className='ticker-container'>
-            <Box>
-                <Button onClick={handlePress}>Get</Button>
+        <div className="container full-width">
+            <Box
+                borderWidth='1px'
+                borderRadius='lg'
+                overflow='hidden'
+                p={4} ml={8} mt={5} mr={8}>
+                <Heading
+                    size="md"
+                    mb={4}>
+                    Stock Chart
+                </Heading>
+                <ResponsiveContainer width="100%" height={300}>
+                    <LineChart
+                        data={chartData}
+                        margin={{
+                            top: 5,
+                            right: 30,
+                            left: 20,
+                            bottom: 5
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="date" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Line
+                            type="monotone"
+                            dataKey="close"
+                            stroke="#8884d8"
+                            activeDot={{ r: 8 }}
+                        />
+                    </LineChart>
+                </ResponsiveContainer>
             </Box>
         </div>
     )
 }
 
-export default PricesTable
+export default StockChart
